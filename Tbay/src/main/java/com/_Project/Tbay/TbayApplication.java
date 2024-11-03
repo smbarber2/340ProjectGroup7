d9f6d24f2c0114c12a780bd684e5717269330d22
TSS7 <crtuttle2@uncg.edu>
2024-11-02 23:19:25 -0400

    unbridled hated for git hub

---
+++ Tbay/src/main/java/com/_Project/Tbay/TbayApplication.java
@@ -0,0 +1,13 @@
+package com._Project.Tbay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TbayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TbayApplication.class, args);
+	}
+
+}
---
+++ Tbay/src/test/java/com/_Project/Tbay/TbayApplicationTests.java
@@ -0,0 +1,13 @@
+package com._Project.Tbay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TbayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
