1bdd68d5993d45a8cb1469098c8694cf3bc099c8
Juliet B <jbellionthe1st@gmail.com>
2024-11-14 10:46:24 -0500

    updating mvc user

--- Tbay/src/main/java/com/_Project/Tbay/User/UserController.java
+++ Tbay/src/main/java/com/_Project/Tbay/User/UserController.java
@@ -4,6 +4,7 @@ import com._Project.Tbay.Cart.Cart;
 import com._Project.Tbay.Cart.CartService;
 import org.springframework.stereotype.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
@@ -16,27 +17,55 @@ public class UserController {
     @Autowired
     private CartService cartService;
 
-    @GetMapping("/all")
-    public List<User> getAllUsers(){
-        return service.getAllUsers();
+//    @GetMapping("/all")
+//    public List<User> getAllUsers(){
+//        return service.getAllUsers();
+//    }
+
+    //Returns all users
+//    @GetMapping("/all")
+//    public String getAllUsers(Model model){
+//        model.addAttribute("userList", service.getAllUsers());
+//        return "user-list";
+//    }
+
+
+//    @GetMapping("/{userId}")
+//    public User getOneUser(@PathVariable long userId){return service.getUserById(userId);}
+
+    //Returns information about a specified user, less detailed than admin view
+//    @GetMapping("/{userId}")
+//        public User getOneUser(@PathVariable long userId, Model model){
+//        model.addAttribute("user", service.getUserById(userId));
+//        return "user-details";
+//    }
+
+
+//    @PostMapping("/new")
+//    public List<User> addNewUser(@RequestBody User user){
+//        Cart cart = new Cart();
+//        cartService.addNewCart(cart);
+//        user.setCartId(cart.getCartId());
+//        service.addNewUser(user);
+//        return service.getAllUsers();
+//    }
+
+    //Shows the create account form page
+    @PostMapping("/createform")
+    public String showCreateForm(){
+        return "create-acct";
     }
 
-    @GetMapping("/{userId}")
-    public User getOneUser(@PathVariable long userId){return service.getUserById(userId);}
-
-    @PostMapping("/new")
-    public List<User> addNewUser(@RequestBody User user){
-        Cart cart = new Cart();
-        cartService.addNewCart(cart);
-        user.setCartId(cart.getCartId());
-        service.addNewUser(user);
-        return service.getAllUsers();
+//    @PutMapping("/update/{userId}")
+//    public User updateUser(@PathVariable long userId, @RequestBody User user) {
+//        service.updateUser(userId, user);
+//        return service.getUserById(userId);
+//    }
+    @PostMapping("/update")
+    public String updateUser(User user) {
+        service.saveUser(user);
+        return "redirect:/profile/" + user.getUserId();
     }
 
-    @PutMapping("/update/{userId}")
-    public User updateUser(@PathVariable long userId, @RequestBody User user) {
-        service.updateUser(userId, user);
-        return service.getUserById(userId);
-    }
 
 }
