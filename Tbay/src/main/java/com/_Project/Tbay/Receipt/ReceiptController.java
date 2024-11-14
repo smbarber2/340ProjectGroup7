a9ce55f3718f270881163ac9eeed856c37281370
Juliet B <jbellionthe1st@gmail.com>
2024-11-14 09:57:32 -0500

    mvc branch

--- Tbay/src/main/java/com/_Project/Tbay/Receipt/ReceiptController.java
+++ Tbay/src/main/java/com/_Project/Tbay/Receipt/ReceiptController.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/receipts")
 public class ReceiptController {
     @Autowired
--- Tbay/src/main/java/com/_Project/Tbay/Report/ReportController.java
+++ Tbay/src/main/java/com/_Project/Tbay/Report/ReportController.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/reports")
 
 public class ReportController {
