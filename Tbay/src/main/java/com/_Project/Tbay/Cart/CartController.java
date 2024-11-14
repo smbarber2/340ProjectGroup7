25ca09d8e33b5145d50ba63f03d11082e90fbc7f
TSS7 <crtuttle2@uncg.edu>
2024-11-14 17:11:43 -0500

    THE PAIN AHHHHHH

--- Tbay/src/main/java/com/_Project/Tbay/Cart/CartController.java
+++ Tbay/src/main/java/com/_Project/Tbay/Cart/CartController.java
@@ -1,10 +1,18 @@
 package com._Project.Tbay.Cart;
 
+import com._Project.Tbay.Listing.Listing;
+import com._Project.Tbay.Listing.ListingService;
 import com._Project.Tbay.User.User;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.ObjectInputStream;
+import java.sql.Blob;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -12,6 +20,10 @@ import java.util.List;
 public class CartController {
     @Autowired
     private CartService service;
+    @Autowired
+    private ListingService listingService;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     @GetMapping("/all")
     public String getAllCarts(Model model){
@@ -20,10 +32,47 @@ public class CartController {
         return "cartList";
     }
 
+    /*@GetMapping("/all")
+    public List<Cart> getAllCarts(){
+        return service.getAllCarts();
+    }*/
+
+    @PutMapping("/update/{cartId}")
+    public Cart updateCart(@PathVariable long cartId, @RequestBody Cart cart) {
+        service.updateCart(cartId, cart);
+        return service.getCartById(cartId);
+    }
+
     @GetMapping("/{cartId}")
     public String getCart(@PathVariable long cartId, Model model){
+
+        List<Integer> deserializedList = new ArrayList<>();
+        List<Listing> list = new ArrayList<>();
+
         model.addAttribute("cart", service.getCartById(cartId));
         model.addAttribute("title", "Cart:"+cartId);
+
+        try{
+            Blob blobData = jdbcTemplate.queryForObject(
+                    "SELECT cart_list FROM carts WHERE cart_id= ?",
+                    new Object[]{cartId},
+                    (rs, rowNum) -> rs.getBlob("cart_list")
+            );
+
+            byte[] data = blobData.getBytes(1, (int) blobData.length());
+
+            try(ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(data))) {
+                deserializedList = (ArrayList<Integer>) ois.readObject();
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        for(int val: deserializedList){
+            list.add(listingService.getListingById(val));
+        }
+
+        model.addAttribute("listingList", list);
+
         return "checkout";
     }
 
@@ -31,6 +80,12 @@ public class CartController {
     public void addNewCart(@RequestBody Cart cart) {
         service.addNewCart(cart);
     }
+
+    /*@PostMapping("/new")
+    public List<Cart> addNewCart(@RequestBody Cart cart) {
+        service.addNewCart(cart);
+        return service.getAllCarts();
+    }*/
 
     @GetMapping("/update/{cartId}")
     public void showUpdateCart(@PathVariable long cartId, Model model) {
